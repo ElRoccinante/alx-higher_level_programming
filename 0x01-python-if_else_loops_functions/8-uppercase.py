c7ac736ff9ebd28d3bcb75c724b06a79ca75ef55
Alae Chouiyakh <alaechouiye@gmail.com>
2023-06-06 13:49:44 -0700

    task-8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z')+1):
-    if chr(i) not in ['q', 'e']:
-        print("{}".format(chr(i)), end='')
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
