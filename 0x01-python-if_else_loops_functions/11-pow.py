b9f54b38c0ef0fa3b3afb1c930e3416230e725de
Alae Chouiyakh <alaechouiye@gmail.com>
2023-06-06 15:50:16 -0700

    task-11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-def power(a, b):
-    return a ** b
-
-pow_func = __import__('11-pow').pow
-
-print(round(pow_func(2, 2), 2))
-print(round(pow_func(98, 2), 2))
-print(round(pow_func(98, 0), 2))
-print(round(pow_func(100, -2), 2))
-print(round(pow_func(-4, 5), 2))
+def pow(a, b):
+    result = 1
+    if b < 0:
+        a = 1 / a
+        b = -b
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
+    return result
+print(pow(2, 2))    # Output: 4
+print(pow(98, 2))   # Output: 9604
+print(pow(98, 0))   # Output: 1
+print(pow(100, -2)) # Output: 0.0001
+print(pow(-4, 5))   # Output: -1024
