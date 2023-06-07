a1de1c106c9d2762229da280bb13a73203a485f0
Alae Chouiyakh <alaechouiye@gmail.com>
2023-06-06 17:36:24 -0700

    task-11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def pow(a, b):
-    if b >= 0:
-        result = 1
-        for _ in range(b):
-            result *= a
-    else:
-        result = 1.0
-        for _ in range(-b):
-            result /= a
-    return result
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+    return(a ** b)
