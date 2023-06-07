3d0af01b7dd1002aa00674341bd49b277d44a4c7
Alae Chouiyakh <alaechouiye@gmail.com>
2023-06-06 18:01:47 -0700

    task-13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,105 +1,36 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
-/**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
+listint_t *insert_node(listint_t **head, int number)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+listint_t *new_node = malloc(sizeof(listint_t));
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
-
-    return (n);
+if (new_node == NULL)
+{
+	perror("Memory allocation failed");
+	return (NULL);
 }
 
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+	new_node->n = number;
+	new_node->next = NULL;
+
+	if (*head == NULL || number < (*head)->n)
 {
-    listint_t *new;
-    listint_t *current;
+	new_node->next = *head;
+	*head = new_node;
+	}
+	else
+{
+	listint_t *current = *head;
 
-    current = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
-    else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
-
-    return (new);
+	while (current->next != NULL && current->next->n < number)
+	{
+	current = current->next;
+	}
+	new_node->next = current->next;
+	current->next = new_node;
 }
 
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
-}
-
-/**
- * main - create list and test insert allocations
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-
-    printf("-----------------\n");
-
-    insert_node(&head, 27);
-
-    print_listint(head);
-
-    free_listint(head);
-
-    return (0);
+	return (new_node);
 }
