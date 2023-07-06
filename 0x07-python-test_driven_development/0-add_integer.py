b03b916bcea90ce5ecaa100889e973e9e004d252
Alae Chouiyakh <alaechouiye@gmail.com>
2023-07-06 04:52:54 -0700

    n

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """Function add integers
+
+    Args:
+        a: integer.
+        b: integer, default 98
+
+    Raises:
+        TypeError: if a, b are not int, float
+
+    Returns:
+        The sum of a and b
+    """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
