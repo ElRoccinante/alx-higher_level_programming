dd476873a9c0fb56a0d7a7890f7602cdec0e1b22
ElRoccinante <alaechouiye@gmail.com>
2023-08-16 15:54:17 -0700

    sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- creat database
+create database if not exists hbtn_0d_2;
+CREATE USER if not exists 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
