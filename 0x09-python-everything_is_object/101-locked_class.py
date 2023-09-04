fc3293b41b91026b70446bb4394af2fd5045aa66
Alae Chouiyakh <125565124+ElRoccinante@users.noreply.github.com>
2023-09-04 21:27:26 +0200

    Delete 0x09-python-everything_is_object directory
    remove

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Module for LockedClass"""
-
-
-class LockedClass:
-    """A locked class that only lets the user create the instance
-    attribute 'first_name'"""
-    __slots__ = ['first_name']
