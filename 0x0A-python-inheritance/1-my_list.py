b4ed345a3a45dd2697cb145dcc48a6127b1f2ae1
ElRoccinante <alaechouiye@gmail.com>
2023-09-11 05:39:16 -0700

    j

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python
-"""define an subclass or child list class Mylist"""
+#!/usr/bin/python3
+"""
+0x0A. Python - Inheritance
+"""
 
 
-class Mylist(list):
-    """class Mylist that inherits from list"""    
-    
+class MyList(list):
+    """The class MyList that inherits from list:
+        Public instance method:
+        - def print_sorted(self):
+                # that prints the list, sorted (ascending sort)s
+                # asssuming that elements are of type int
+    """
 
     def print_sorted(self):
-        """methode for print sorted list"""
-        print(sorted(self))
+        """Public instance method"""
+        sorted_list = sorted(self, key=None, reverse=False)
+        print(sorted_list)
