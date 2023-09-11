4e05fb125b41cf667a05fa9c81a266f762eb5efe
ElRoccinante <alaechouiye@gmail.com>
2023-09-11 05:18:47 -0700

    h

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """It returns list methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+
+class Mylist(list):
+    """class Mylist that inherits from list"""    
+    
+
+    def print_sorted(self):
+        """methode for print sorted list"""
+        print(sorted(self))
