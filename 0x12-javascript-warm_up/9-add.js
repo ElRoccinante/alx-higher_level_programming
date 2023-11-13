2b96ad449e9f5dec1c4c17210407ccd44e6e8c4a
ElRoccinante <alaechouiye@gmail.com>
2023-11-13 14:27:18 -0800

    addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-const args = process.argv;
-const a = parseInt(args[2], 10);
-const b = parseInt(args[3], 10);
-function add (a, b) {
-  return a + b;
+const args = process.argv.slice(2);
+let row = '';
+if (!isNaN(args[0]) && args[0] >= 0) {
+  for (let i = 0; i < args[0]; i++) {
+    for (let j = 0; j < args[0]; j++) {
+      row += 'X';
+    }
+    console.log(row);
+    row = '';
+  }
+} else if (isNaN(args[0])) {
+  console.log('Missing size');
 }
-console.log(add(a, b));
