c8873e09f0d3f34bf03ba4487e64a395309e475c
ElRoccinante <alaechouiye@gmail.com>
2023-11-19 13:26:18 -0800

    correction

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""
+Script containing State class definition and declarative_base () instance
+"""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from model_city import City
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Class representing the states table"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    """Represents a city for MySQL database
+
+    Attributes:
+        id (str): The city's id
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
