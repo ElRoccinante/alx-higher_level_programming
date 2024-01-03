953313d095e4dad6b17afd2d54682199408daeee
ElRoccinante <alaechouiye@example.com>
2024-01-03 15:46:15 -0800

    script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,33 +1,22 @@
 #!/usr/bin/node
+const util = require('util');
 const request = require('request');
-const id = process.argv[2];
-const endpoint = `https://swapi-api.alx-tools.com/api/films/${id}`;
-const opt = { method: 'GET' };
-/**
- * * Get name of a char from starwars using promises
- * * @param {*} url url str
- * * @returns Promise<str> (name)
- * * Made by MEGA
- * */
-const getName = (url) => new Promise((resolve, reject) => {
-	  request(url, opt, (err, { statusCode: code, body: res }) => {
-		      if (err) reject(console.log(err));
-		      if (code === 200) {
-			            resolve(JSON.parse(res).name);
-			          } else {
-					        resolve(console.log(`Error code: ${code}`));
-					      }
-		    });
-});
 
-request(endpoint, opt, (err, { statusCode, body }) => {
-	  if (err) return console.log(err);
-	  if (statusCode === 200) {
-		      const { characters } = JSON.parse(body);
-		      const names = characters.map(url => getName(url));
-		      Promise.all(names)
-		        .then(names => names.forEach(name => console.log(name)));
-		    } else {
-			        console.log(`Error code: ${statusCode}`);
-			      }
+const id = process.argv[2];
+const reqPromis = util.promisify(request);
+
+request.get(`https://swapi-api.alx-tools.com/api/films/${id}/`, async (err, resp) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const people = JSON.parse(resp.body).characters;
+    for (const i in people) {
+      try {
+        const respence = await reqPromis(people[i]);
+        console.log(JSON.parse(respence.body).name);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 });
